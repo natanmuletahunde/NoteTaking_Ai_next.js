3bb77136eb1d9896cb51d10b9f2fee31e73a23b8
pivnati <natanmuleta911@gmail.com>
2025-07-22 22:22:38 -0700

    create the the utils file in the styles folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,13 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Header/>
-          {children}
+          <div className="flex min-h-screen w-full flex-col">
+            <Header />
+            <main className="flex flex-1 flex-col px-10 xl:px-8">
+              {children}
+            </main>
+          </div>
+
           <Toaster />
         </ThemeProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='bg-white'>
+    <div>
        Homepage
     </div>
   )

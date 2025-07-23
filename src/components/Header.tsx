c90651bbb317cdc98d6ae3b912bc8f65de283045
pivnati <natanmuleta911@gmail.com>
2025-07-23 03:53:54 -0700

    add the leading-6 in the vertical height of the words

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
+import { shadow } from "@/styles/utils";
 const Header = () => {
   return (
-    <div>
-      <Link href="/">
+    <header className="relative flex h-24 w-full items-center  justify-between bg-popover px-3
+    sm:px-6"
+    style={{
+        boxShadow:shadow
+    }}>
+      <Link  className="flex items-end gap-2" href="/">
         <Image
           src="/goatius.png"
           height={60}
@@ -12,8 +17,11 @@ const Header = () => {
           className="rounded-full"
           priority
         />
+     <h1 className="flex flex-col pb-1 text-2xl font-semibold leading-6">
+        GOAT <span>Notes</span>
+     </h1>
       </Link>
-    </div>
+    </header>
   );
 };
 

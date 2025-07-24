f2a9cd901af9ebf5c1c014e123e81cdb26e52bef
pivnati <natanmuleta911@gmail.com>
2025-07-23 23:02:09 -0700

    create the loginpage

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+"use client";
 
-const LogoutButton = () => {
+import { Loader2 } from "lucide-react";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { toast } from "sonner"; // ✅ Use Sonner here
+import { useRouter } from "next/navigation";
+import { logOutAction } from "@/actions/users";
+
+function LogOutButton() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const handleLogOut = async () => {
+    setLoading(true);
+
+    const { errorMessage } = await logOutAction();
+
+    if (!errorMessage) {
+      router.push(`/?toastType=logOut`);
+    } else {
+      toast.error("Logout Failed", {
+        description: errorMessage,
+      });
+    }
+
+    setLoading(false);
+  };
+
   return (
-    <div>
-       LogoutButton   
-    </div>
-  )
+    <Button
+      variant="outline"
+      onClick={handleLogOut}
+      disabled={loading}
+      className="w-24"
+    >
+      {loading ? <Loader2 className="animate-spin" /> : "Log Out"}
+    </Button>
+  );
 }
 
-export default LogoutButton
+export default LogOutButton;

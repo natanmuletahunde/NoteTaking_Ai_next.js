3360cbfa5fec9d127476be5de216791d245bb582
pivnati <natanmuleta911@gmail.com>
2025-08-01 00:17:58 -1200

    install  the sidebar and fix the error in the header file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,24 @@
-import Link from "next/link";
-import Image from "next/image";
 import { shadow } from "@/styles/utils";
+import Image from "next/image";
+import Link from "next/link";
 import { Button } from "./ui/button";
 import DarkModeToggle from "./DarkModeToggle";
-const Header = () => {
-  const user = null; // Replace with actual user state or context
+import LogOutButton from "./LogoutButton";
+import { getUser } from "@/auth/server";
+import { SidebarTrigger } from "./ui/sidebar";
+
+async function Header() {
+  const user = await getUser();
+
   return (
     <header
-      className="bg-popover relative flex h-24 w-full items-center justify-between px-3 sm:px-6"
+      className="bg-popover relative flex h-24 w-full items-center justify-between px-3 sm:px-8"
       style={{
         boxShadow: shadow,
       }}
     >
+      <SidebarTrigger className="absolute left-1 top-1" />
+
       <Link className="flex items-end gap-2" href="/">
         <Image
           src="/goatius.png"
@@ -21,27 +28,31 @@ const Header = () => {
           className="rounded-full"
           priority
         />
+
         <h1 className="flex flex-col pb-1 text-2xl font-semibold leading-6">
           GOAT <span>Notes</span>
         </h1>
       </Link>
+
       <div className="flex gap-4">
         {user ? (
-          "Logout"
+          <LogOutButton />
         ) : (
           <>
             <Button asChild>
-              <Link className="hidden sm:block" href="/sign-up">Sign Up</Link>
+              <Link href="/sign-up" className="hidden sm:block">
+                Sign Up
+              </Link>
             </Button>
-            <Button asChild>
+            <Button asChild variant="outline">
               <Link href="/login">Login</Link>
             </Button>
           </>
         )}
-        <DarkModeToggle/>
+        <DarkModeToggle />
       </div>
     </header>
   );
-};
+}
 
 export default Header;
